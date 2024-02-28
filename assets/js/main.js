e22167d465f806b06184669d15947a6465da6b17
Roberio Nascimento <roberio_dsn@hotmail.com>
2024-02-28 00:32:32 -0300

    Realizando uma requisição Http via JavaScript

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,11 @@
 
+const offset = 0
+const limit = 10
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-cont url = 
+fetch(url).then(function (response) {
+    console.log(response)
+})
+
+const x = 10 + 10
+console.log(x )
