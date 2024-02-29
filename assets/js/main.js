7fe6a6ac892784908e636fac49b4450bf6a57270
Roberio Nascimento <roberio_dsn@hotmail.com>
2024-02-28 23:30:02 -0300

    Utilizando arrow function

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,9 +3,16 @@ const offset = 0
 const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-fetch(url).then(function (response) {
-    console.log(response)
+fetch(url)
+    .then((response) => {
+        return response.json()       
 })
-
-const x = 10 + 10
-console.log(x )
+.then(function (jsonBody)     {
+        console.log(jsonBody)
+})
+.catch(function (error) {
+    console.error(error)
+})
+.finally(function () {
+    console.log('Requisição concluída!')
+})
