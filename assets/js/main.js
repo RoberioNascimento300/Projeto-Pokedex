4043b42ad2754143a436fd5d92b0964fa383db2e
Roberio Nascimento <roberio_dsn@hotmail.com>
2024-02-28 23:35:52 -0300

    arrow function mais reduzido

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,9 +4,8 @@ const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
 fetch(url)
-    .then((response) => {
-        return response.json()       
-})
+    .then((response) => response.json())       
+
 .then(function (jsonBody)     {
         console.log(jsonBody)
 })
