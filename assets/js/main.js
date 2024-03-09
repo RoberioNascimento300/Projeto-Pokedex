2cc1e952f1055794fe65c855b93f9ae26bb0d454
Roberio Nascimento <roberio_dsn@hotmail.com>
2024-03-09 17:13:01 -0300

    Criando mecanismo de paginação

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,39 @@
+const pokemonList = document.getElementById('pokemonList') //Eu tô vindo no meu Html que foi renderizado no meu browser, eu tô pegando a nossa lista de pokemon e ATRIBUINDO a uma variável: pokemonList, então a partir desse momento eu tenho acesso programático ao objeto
+const loadMoreButton = document.getElementById('loadMoreButton')
+const limit_main = 5 //Precisa renomear a declaração pois já tem a mesma variável declarada no poke-api.js
+let offset_main = 0 //Precisa renomear a declaração pois já tem a mesma variável declarada no poke-api.js
 
-function convertPokemonToLi(pokemon) {
-  return `
-    <li class="pokemon ${pokemon.type}">
-        <span class="number">#${pokemon.number}</span>
-        <span class="name">${pokemon.name}</span>
-    
-        <div class="detail">
-            <ol class="types">
-                ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')} 
-            </ol>
+function loadPokemonItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map((pokemon) => `
+            <li class="pokemon ${pokemon.type}">
+                <span class="number">#${pokemon.number}</span>
+                <span class="name">${pokemon.name}</span>
 
+                <div class="detail">
+                    <ol class="types">
+                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                    </ol>
 
-            <img src="${pokemon.photo}" 
-                 alt="${pokemon.name}">
-        </div>
-    </li>
-`
+                    <img src="${pokemon.photo}"
+                        alt="${pokemon.name}">
+                </div>
+            </li>
+        `).join('')
+        
+        pokemonList.innerHTML += newHtml
+    })
 }
 
-const pokemonList = document.getElementById('pokemonList') //Eu tô vindo no meu Html que foi renderizado no meu browser, eu tô pegando a nossa lista de pokemon e ATRIBUINDO a uma variável: pokemonList, então a partir desse momento eu tenho acesso programático ao objeto
- 
-pokeApi.getPokemons().then((pokemons = []) => {
-   pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
-   //Olha, pega a lista de pokemons, MAPEIA os pokemons CONVERTENDO esta lista para Li
-   //E agora JUNTA todos esses Li sem separador nenhum 
-   //Isso vai virar um Html NOVO
-   //E eu vou CONCATENAR com o html antigo que eu tinha
+loadPokemonItens(offset_main, limit_main)
+
+loadMoreButton.addEventListener('click', () => {
+    offset_main += limit_main //INCREMENTO do offset
+    loadPokemonItens(offset_main, limit_main)
 })
+
+
+
+
 
 
