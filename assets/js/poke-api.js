285492270f2a9240de712513b438d0f7327f6f0d
Roberio Nascimento <roberio_dsn@hotmail.com>
2024-03-21 20:16:07 -0300

    Adicionando funcionalidade para exibir os detalhes do pokemon

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,40 +1,55 @@
-const offset = 0
-const limit = 10
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-
-const pokeApi = {} //Declarando o objeto pokeApi
-
-function convertPokeApiDetailToPokemon(pokeDetail) {
-    const pokemon = new Pokemon()
-    pokemon.number = pokeDetail.id
-    pokemon.name = pokeDetail.name
-
-    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
-    const [type] = types
-
-    pokemon.types = types
-    pokemon.type = type
-
-    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
-
-    return pokemon
+const pokeApi = {
+     
 }
+
+function convertPokeApiDetail(pokeDetail) {
+
+        const pokemon = new Pokemon()
+
+        pokemon.id = pokeDetail.id
+        pokemon.name = pokeDetail.name
+
+        const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+        const [type] = types
+        pokemon.types = types
+        pokemon.type = type
+
+        pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+
+        const abilities = pokeDetail.abilities.map((abilitiesName) => abilitiesName.ability.name)
+        const [ability] = abilities
+        pokemon.abilities = abilities
+        pokemon.ability = ability
+
+        const stats = pokeDetail.stats.map((statValue) => statValue.base_stat)
+        const [baseStat] = stats
+        pokemon.stats = stats
+        pokemon.baseStat = baseStat
+
+        const stat = pokeDetail.stats.map((statName) => statName.stat.name)
+        const [nameStats] = stat
+        pokemon.stat = stat
+        pokemon.nameStats = nameStats 
+
+
+        return pokemon
+}
+
 
 pokeApi.getPokemonDetail = (pokemon) => {
-   return fetch(pokemon.url)
-            .then((response) => response.json())
-            .then(convertPokeApiDetailToPokemon)         
+  return fetch(pokemon.url).then((response) => response.json())
+  .then(convertPokeApiDetail)
 }
+
 
 pokeApi.getPokemons = (offset = 0, limit = 5) => {
-    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
     
-    return fetch(url) // quero retornar a lista de pokemon pronta. Para que ninguém fora do bloco aq tenha que entender como manipular uma resposta de uma pokeApi
-        .then((response) => response.json()) //CONVERTENDO o response para json
-        .then((jsonBody) => jsonBody.results)
-        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail)) //MAPEANDO a lista de pokemons em uma lista de requisições do detalhe do pokemon
-        .then((detailRequests) => Promise.all(detailRequests)) //Estamos esperando que TODAS as requisições terminem
-        .then((pokemonsDetails) => pokemonsDetails) //Vai imprimir a lista de detalhes do pokemon)
+   const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+   return fetch(url)
+   .then((response) => response.json())
+   .then((jsonBody) => jsonBody.results)
+   .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+   .then((detailRequest) => Promise.all(detailRequest))
+   .then((pokemonDetails) => pokemonDetails)
 }
-
